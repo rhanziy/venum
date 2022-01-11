d83c2850a97313532971583a39f1626db529ca8e
rhanziy <thdfksdl0029@gmail.com>
2022-01-12 01:29:36 +0900

    22.01.12. edit

---
+++ js/sub01script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  
+    var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+        rotate: 30,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+        },
+        pagination: {
+        el: ".swiper-pagination",
+        }
+    });
+
+
+});
